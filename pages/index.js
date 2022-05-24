942f71c1cb24dcf5e86982e7cd009015805174af
Daniel Heater <dheater@securelink.com>
2022-05-24 09:07:13 -0500

    Add form for entering the server URL

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import ServerUrlForm from "../components/ServerUrlForm";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Websocket Analyzer</title>
-        <meta name="description" content="A tool for analyzing websocket data" />
-        {/*<link rel="icon" href="/favicon.ico" />*/}
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Websocket Analyzer</title>
+                <meta name="description" content="A tool for analyzing websocket data"/>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Websocket Analyzer
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Websocket Analyzer
+                </h1>
 
-        <p className={styles.description}>
-          Get started by connecting to a websocket server.
-        </p>
+                <p className={styles.description}>
+                    Get started by connecting to a websocket server.
+                </p>
 
-      </main>
-    </div>
-  )
+                <ServerUrlForm/>
+
+            </main>
+        </div>
+    )
 }
